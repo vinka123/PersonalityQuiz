aae254507e55c1498e57be1828f3728f1c1c2a3a
vinka123 <ktnvctr123@gmail.com>
2020-04-10 19:19:01 +0300

    Add outdoor or indoor in html file

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 alert('A simple personality quiz i created');
 var vert=prompt('Are you an introvert or extrovert?');
 var soc=prompt('Are you social or anti-social?');
-var mvt=prompt('Do you like the indoors or outdoors?');
-if(vert==='introvert' && mvt==='indoors' ||soc==='anti-social') {
+var mvt=prompt('outdoor or indoor?');
+if(vert==='introvert' && mvt==='indoor' ||soc==='anti-social') {
 alert('You tend to show the same characters as a LEOPARD *^*');
 }else if(vert==='extrovert' && mvt==='outdoor' || soc==='social') {
 alert('Roarrr!!A LION');
