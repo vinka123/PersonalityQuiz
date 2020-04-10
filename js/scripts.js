1105c2f5d0ff3bc64ac9236d354a316edd942d1e
vinka123 <ktnvctr123@gmail.com>
2020-04-10 13:35:32 +0300

    Add scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+alert('A simple personality quiz i created');
+var vert=prompt('Are you an introvert or extrovert?');
+var soc=prompt('Are you social or anti-social?');
+var mvt=prompt('Do you like the indoors or outdoors?');
+if(vert==='introvert' && mvt==='indoors' ||soc==='anti-social') {
+alert('You tend to show the same characters as a LEOPARD *^*');
+}else if(vert==='extrovert' && mvt==='outdoor' || soc==='social') {
+alert('Roarrr!!A LION');
+} else {
+alert('Wuwu!You bark often as the DOG you are(:');
+}
